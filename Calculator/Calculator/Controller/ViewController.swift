4cf46861ab07f305dd5f871fa641455784ab2bbf
mireu79 <https://github.com/mireu79>
2023-10-16 15:01:16 +0900

    feat: resetBtton함수구현

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -9,14 +9,19 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var operandLabel: UILabel!
     @IBOutlet weak var operatorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        operandLabel.text = "0"
+        operatorLabel.text = ""
     }
 
-
+    
+    @IBAction func resetButton(_ sender: Any) {
+        stackView.arrangedSubviews.forEach{ $0.removeFromSuperview() }
+    }
 }
 
